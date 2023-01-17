bc8a87efa4d50e1f5633e8f38db2b7514b7e8092
nelumbo <remi.chambiet@laposte.net>
2023-01-17 11:07:32 +0100

    supression des commentaire inutile

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -21,8 +21,3 @@ function Gallery(){
 
 }export default Gallery
 
-{/* <Cards urlext="https://selnir.github.io/Front-End-Fisheye/" image="./Fisheye.jpg"></Cards>
-    <Cards urlext="https://selnir.github.io/GameOn-website-FR/" image="./game_on_site_screeenshots.jpg"></Cards>
-    <Cards urlext="https://selnir.github.io/kasa/" image="./Kasa_screenshots.jpg"></Cards>
-    <Cards urlext="https://selnir.github.io/Projet-7_les_petits_plats/" image="./Les_petits_plats_screenchots.jpg"></Cards>
-    <Cards urlext="https://selnir.github.io/ohmyfood/" image="./ohmyfood_screenshots.jpg"></Cards> */}
