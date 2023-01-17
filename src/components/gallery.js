3343043a8eb5bf728c6190dde112ab063c2c6280
nelumbo <remi.chambiet@laposte.net>
2023-01-17 11:06:00 +0100

    mise a jour de la gallery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -5,10 +5,10 @@ import Next from './button_next';
 
 function Gallery(){
 
-    const feed=[<a href="https://selnir.github.io/Front-End-Fisheye/" target="_blank" rel="noopener noreferrer"><img src="./Fisheye.jpg" alt="Fisheye"></img></a>,
-   <a href="https://selnir.github.io/GameOn-website-FR/" target="_blank" rel="noopener noreferrer"> <img src="./game_on_site_screeenshots.jpg" alt="GameOnSite"></img></a>,
-    <a href="https://selnir.github.io/kasa/" target="_blank" rel="noopener noreferrer"><img src="./Kasa_screenshots.jpg" alt="Kasa"></img></a>,
+    const feed=[<a href="https://selnir.github.io/kasa/" target="_blank" rel="noopener noreferrer"><img src="./Kasa_screenshots.jpg" alt="Kasa"></img></a>,
     <a  href="https://selnir.github.io/Projet-7_les_petits_plats/" target="_blank" rel="noopener noreferrer"><img src="./Les_petits_plats_screenchots.jpg" alt="LesPetitsPlats"></img></a>,
+    <a href="https://selnir.github.io/Front-End-Fisheye/" target="_blank" rel="noopener noreferrer"><img src="./Fisheye.jpg" alt="Fisheye"></img></a>,
+   <a href="https://selnir.github.io/GameOn-website-FR/" target="_blank" rel="noopener noreferrer"> <img src="./game_on_site_screeenshots.jpg" alt="GameOnSite"></img></a>,
     <a href="https://selnir.github.io/ohmyfood/" target="_blank" rel="noopener noreferrer"><img src="./ohmyfood_screenshots.jpg" alt="Ohmyfood"></img></a>,
 
 
